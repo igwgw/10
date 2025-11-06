6c0e011f57cdb31a8d9afafb6d4643323b6289da
igwgw <hanseul0204@sookmyung.ac.kr>
2025-11-06 15:45:23 +0900

    05

--- main.c
+++ main.c
@@ -5,9 +5,23 @@
 
 void main(void)
 {
-    char str[30]="happy C prograaming";
+    char input[100];
+    FILE* fp;
+    int i;
     
-    printf("string %s length : %i\n",str,strlen(str));
+    //fopen
+    fp= fopen("sample.txt","w");
+    //fprintf
+    
+    for(i=0;i<3;i++){
+    printf("input a word: ");
+    scanf("%s",input);//¹è¿­Àº&¾È¾¸
+    fprintf(fp, "%s\n",input);
+} 
+     
+    //fclose
+    fclose(fp);
+
   
   system("PAUSE");	
   return 0;
